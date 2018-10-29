9a32f6c0a6769b403ea1ddd2a9c483e0b6c6c19d
gmariano <giuseppe.mariano@albelli.com>
2018-10-29 16:41:45 +0100

    Store teams as json

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using WindowsFormsApp1.Model;
 using Microsoft.Office.Interop.Excel;
+using Newtonsoft.Json;
+using Application = Microsoft.Office.Interop.Excel.Application;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        const string ROSE_JSON_PATH = @"C:\Users\gmariano\Desktop\Fantagazzetta\_rose.json";
+
 
         public Form1()
         {
@@ -18,21 +23,62 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //var fname = "";
-            //var fdlg = new OpenFileDialog();
-            //fdlg.Title = "Excel File Dialog";
-            //fdlg.InitialDirectory = @"c:\";
-            //fdlg.Filter = "All files (*.*)|*.*|All files (*.*)|*.*";
-            //fdlg.FilterIndex = 2;
-            //fdlg.RestoreDirectory = true;
-            //if (fdlg.ShowDialog() == DialogResult.OK)
-            //{
-            //    fname = fdlg.FileName;
-            //}
+            List<Team> teams = null;
+            if (File.Exists(ROSE_JSON_PATH))
+            {
+                using (var streamReader = new StreamReader(ROSE_JSON_PATH))
+                {
+                    var json = streamReader.ReadToEnd();
+                    teams = JsonConvert.DeserializeObject<List<Team>>(json);
+                }
+            }
+            else
+            {
+                teams = GetTeamsFromExcel();
+            }
 
-            var xlApp = new Microsoft.Office.Interop.Excel.Application();
-            var xlWorkbook = xlApp.Workbooks.Open(@"C:\Users\gmariano\Desktop\Fantagazzetta\_rose.xlsx");
-            _Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            FillDataGrid(teams);
+        }
+
+        private void FillDataGrid(IReadOnlyCollection<Team> teams)
+        {
+            dataGridView1.ColumnCount = 5;
+            dataGridView1.RowCount = teams.Sum(x => x.Players.Count) + 20;
+
+            var i = 0;
+            foreach (var team in teams)
+            {
+                dataGridView1.Rows[i++].Cells[0].Value = team.Name;
+                team.Players.ForEach(
+                    x =>
+                    {
+                        dataGridView1.Rows[i].Cells[0].Value = x.Name;
+                        dataGridView1.Rows[i].Cells[1].Value = x.Role;
+                        dataGridView1.Rows[i].Cells[2].Value = x.RealTeam;
+                        i++;
+                    });
+                i++;
+            }
+        }
+
+        private static List<Team> GetTeamsFromExcel()
+        {
+            var excelPath = "";
+            var fdlg = new OpenFileDialog
+            {
+                Title = "Select Excel",
+                InitialDirectory = @"c:\", Filter = "All files (*.*)|*.*|All files (*.*)|*.*",
+                FilterIndex = 2,
+                RestoreDirectory = true
+            };
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                excelPath = fdlg.FileName;
+            }
+
+            var xlApp = new Application();
+            var xlWorkbook = xlApp.Workbooks.Open(excelPath);
+            var xlWorksheet = xlWorkbook.Sheets[1];
             var xlRange = xlWorksheet.UsedRange;
 
             var rowCount = xlRange.Rows.Count;
@@ -67,55 +113,46 @@ namespace WindowsFormsApp1
 
             foreach (var coordinates in playersCoordinates)
             {
-                var teamName = xlRange.Cells[coordinates.StartCell.Row-1, coordinates.StartCell.Column].Value2.ToString();
+                var teamName = xlRange.Cells[coordinates.StartCell.Row - 1, coordinates.StartCell.Column].Value2.ToString();
                 var team = new Team { Name = teamName };
-                for (var rowIndex = coordinates.StartCell.Row+1; rowIndex < coordinates.EndCell.Row; rowIndex++)
+                for (var rowIndex = coordinates.StartCell.Row + 1; rowIndex < coordinates.EndCell.Row; rowIndex++)
                 {
                     Enum.TryParse(xlRange.Cells[rowIndex, coordinates.StartCell.Column].Value2, out Role role);
                     string playerName = xlRange.Cells[rowIndex, coordinates.StartCell.Column + 1].Value2;
                     string realTeam = xlRange.Cells[rowIndex, coordinates.StartCell.Column + 2].Value2;
                     team.Players.Add(new Player(playerName, role, realTeam));
                 }
+
                 teams.Add(team);
             }
 
-            dataGridView1.ColumnCount = colCount;
-            dataGridView1.RowCount = teams.Sum(x=>x.Players.Count)+20;
-
-            var i = 0;
-            foreach (var team in teams)
+            using (var file = File.CreateText(ROSE_JSON_PATH))
             {
-                dataGridView1.Rows[i++].Cells[0].Value = team.Name;
-                team.Players.ForEach(
-                    x =>
-                    {
-                        dataGridView1.Rows[i].Cells[0].Value = x.Name;
-                        dataGridView1.Rows[i].Cells[1].Value = x.Role;
-                        dataGridView1.Rows[i].Cells[2].Value = x.RealTeam;
-                        i++;
-                    });
-                i++;
+                var serializer = new JsonSerializer();
+                serializer.Serialize(file, teams);
             }
 
-            //cleanup  
+            //cleanup
+            xlWorkbook.Close();
+            xlApp.Quit();
             GC.Collect();
             GC.WaitForPendingFinalizers();
             Marshal.ReleaseComObject(xlRange);
             Marshal.ReleaseComObject(xlWorksheet);
-            xlWorkbook.Close();
             Marshal.ReleaseComObject(xlWorkbook);
-            xlApp.Quit();
             Marshal.ReleaseComObject(xlApp);
+
+            return teams;
         }
     }
 
-    public class PlayersCoordinates
+    internal sealed class PlayersCoordinates
     {
         public CellCoordinates StartCell { get; set; }
         public CellCoordinates EndCell { get; set; }
     }
 
-    public class CellCoordinates
+    internal sealed class CellCoordinates
     {
         public int Row { get; set; }
         public int Column { get; set; }
---
+++ WindowsFormsApp1/WindowsFormsApp1/Model/PlayerRating.cs
@@ -0,0 +1,57 @@
+﻿namespace WindowsFormsApp1.Model
+{
+    public class PlayerRating
+    {
+        public string Code { get; set; }
+        public Role Role { get; set; }
+        public string Name { get; set; }
+        public decimal Voto { get; set; }
+        public int GolFatti { get; set; }
+        public int GolSubiti { get; set; }
+        public int RigoriParati { get; set; }
+        public int RigoriSubiti { get; set; }
+        public int RigoriSegnati { get; set; }
+        public int Autogol { get; set; }
+        public int Ammonizioni { get; set; }
+        public int Espulsioni { get; set; }
+        public int Assist { get; set; }
+        public int AssistDaFermo { get; set; }
+        public int GolVittoria { get; set; }
+        public int GolPareggio { get; set; }
+
+        public decimal VotoFinale
+        {
+            get
+            {
+                if (Voto == 0)
+                {
+                    return 0;
+                }
+
+                var bonusGoalPareggio = Role == Role.P ? 5 : 0.5m;
+                var bonusGoalVittoria = Role == Role.P ? 6m : 1;
+
+                var votoFinale = Voto
+                + (GolFatti * 3)
+                + (GolSubiti * -1)
+                + (RigoriParati * 3)
+                + (RigoriSubiti * -1)
+                + (RigoriSegnati * 3)
+                + (Autogol * -2)
+                + (Ammonizioni * -0.5m)
+                + (Espulsioni * -1)
+                + (Assist * 1)
+                + (AssistDaFermo * 1)
+                + (GolVittoria * bonusGoalVittoria)
+                + (GolPareggio * bonusGoalPareggio);
+
+                if (Role == Role.P && GolSubiti == 0)
+                {
+                    votoFinale = VotoFinale + 0.5m;
+                }
+
+                return votoFinale;
+            }
+        }
+    }
+}
